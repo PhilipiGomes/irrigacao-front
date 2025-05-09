badd0e56b21cf7fb362ad1a692ea97ebbbbc5b56
PhilipiGomes <philipi.rodrigues342@gmail.com>
2025-05-09 07:30:41 -0300

    wow

---
+++ src/static/script.js
@@ -0,0 +1,6 @@
+// Simulação de login com Google
+const googleBtn = document.getElementById('googleLogin');
+googleBtn.addEventListener('click', () => {
+  // Aqui você integraria a API do Google
+  alert('Redirecionando para o login com Google...');
+});
